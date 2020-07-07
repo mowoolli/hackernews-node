c5748c0682733d2689a4b14796781b75d86be338
Marc Woolliscroft <mwoolli@gmail.com>
2020-07-07 11:18:08 -0700

    Add mutation for creating a new post

--- src/index.js
+++ src/index.js
@@ -6,11 +6,24 @@ let links = [{
   description: 'Fullstack tutorial for GraphQL'
 }]
 
+let idCount = links.length
+
 const resolvers = {
   Query: {
     info: () => 'This is the API of a Hackernews Clone',
     feed: () => links,
   },
+  Mutation: {
+    post: (parent, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      }
+      links.push(link)
+      return link
+    }
+  }
 }
 
 const server = new GraphQLServer({
