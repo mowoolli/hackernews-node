e1dab7e0a84a292488fe5734b871fca863962845
Marc Woolliscroft <mwoolli@gmail.com>
2020-07-08 15:27:57 -0700

    Modify post mutation and import subscription

--- src/index.js
+++ src/index.js
@@ -6,12 +6,14 @@ const Query = require('./resolvers/Query')
 const Mutation = require('./resolvers/Mutation')
 const User = require('./resolvers/User')
 const Link = require('./resolvers/Link')
+const Subscription = require('./resolvers/Subscription')
 
 const resolvers = {
   Query,
   Mutation,
   User,
   Link,
+  Subscription,
 }
 
 const prisma = new PrismaClient()
